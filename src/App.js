430a33d4b843ba53d2c990f017a6b72b725a1134
FlorianSdorra <fsd114@posteo.de>
2020-02-25 16:04:52 +0100

    started test searching on deezer search endpoint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { data: ""};
+    this.handlesearch = this.handlesearch.bind(this)
+  }
+
+  handlesearch(){
+    fetch(`https://api.deezer.com/search?q=${searchWord}`)
+    .then(results=>{
+      return results.json();
+    }).then(
+      data =>{
+        this.setState({data: JSON.stringify(data)})
+      }
+    )
+    console.log(this.state)
+  }
+  
+
+  render(){
+    return (
+      <div className="App">
+        {this.state.data}
+        <button onClick={this.handlesearch}></button>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
