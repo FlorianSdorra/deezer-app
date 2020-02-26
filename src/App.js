87c72c347a89179f29c8f4e6f9310d7ee847839c
FlorianSdorra <fsd114@posteo.de>
2020-02-26 15:19:32 +0100

    fetching tracks from deezer and listing first 25 titles

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
 import React from 'react';
 import './App.scss';
+import Track from './components/Track';
 
 class App extends React.Component{
   constructor(props){
     super(props);
-    this.state = { data: ""};
-    this.handlesearch = this.handlesearch.bind(this)
+    this.state = {
+      data:[]
+    }
+  
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.justTitles = this.justTitles.bind(this);
   }
 
-  handlesearch(){
-    fetch(`https://api.deezer.com/search?q=${searchWord}`)
+  handleSubmit(event){
+    event.preventDefault();
+    fetch(`https://api.deezer.com/search?q=${this.state.searchWord}`)
     .then(results=>{
       return results.json();
     }).then(
       data =>{
-        this.setState({data: JSON.stringify(data)})
+        console.log(data)
+        this.setState({data: this.justTitles(data.data)
+        })
       }
     )
-    console.log(this.state)
   }
-  
+
+  justTitles(arr){
+    const newArr = []
+    arr.map(item=>{
+      return newArr.push({track: item.title, artist: item.artist.name, cover: item.album.cover})
+    }
+    )
+    return newArr
+  }
+
+  handleChange(event){
+    console.log(event.target.value)
+    this.setState({searchWord: event.target.value});
+  }
 
   render(){
+    const trackList = this.state.data.map(track=>{
+      return <Track data={track.track}></Track>
+    })
+
+    console.log(this.state)
     return (
       <div className="App">
-        {this.state.data}
-        <button onClick={this.handlesearch}></button>
+        <p></p>
+      <form onSubmit={this.handleSubmit} >
+        <label>
+        Search a track:
+        <input type="text" value={this.state.searchWord} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+        </form>
+        <div className="track-card-container">
+          {trackList} 
+        </div>
       </div>
     );
   }
-  
 }
 
 export default App;
---
+++ src/components/Track.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Track = props =>{
+    return (
+        <div className="track-card">
+            <h2>{props.data}</h2>
+        </div>
+    )
+}
+
+export default Track
